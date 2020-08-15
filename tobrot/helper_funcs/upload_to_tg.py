aec9e30540c35a9e600d4a97941df40e0adc165e
skbaji6 <1@Samreenayaz>
2020-08-15 06:19:24 +0530

    changes

--- tobrot/helper_funcs/upload_to_tg.py
+++ tobrot/helper_funcs/upload_to_tg.py
@@ -65,8 +65,8 @@ async def upload_to_tg(
     if txt.find("rename")>0 and len(txt[txt.find("rename")+7:]) >0 and os.path.isfile(local_file_name):
         rename_text=txt[txt.find("rename")+7:]
         print("BAJ LocFileName Before : "+local_file_name)
-        absName=rename_text+Path(local_file_name).suffix
-        os.rename(local_file_name,os.path.join(os.path.dirname(local_file_name),absName))
+        absName=os.path.join(os.path.dirname(local_file_name),rename_text+Path(local_file_name).suffix)
+        os.rename(local_file_name,absName)
         local_file_name=absName
         print("BAJ LocFileName After : "+local_file_name)
     base_file_name = os.path.basename(local_file_name)
