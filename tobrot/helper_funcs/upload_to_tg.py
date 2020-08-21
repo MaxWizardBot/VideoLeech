a15aedb295b427545700496c2fee36e912a2fe73
skbaji6 <1@Samreenayaz>
2020-08-21 06:07:02 +0530

    rename functionality

--- tobrot/helper_funcs/upload_to_tg.py
+++ tobrot/helper_funcs/upload_to_tg.py
@@ -64,8 +64,8 @@ async def upload_to_tg(
     LOGGER.info(local_file_name)
     LOGGER.info(message.command)
     if message.reply_to_message is not None:
-        txt=getattr(message.reply_to_message,'text','')
-        if txt.find("rename")>-1 and len(txt[txt.find("rename")+7:]) >0 and os.path.isfile(local_file_name):
+        txt=getattr(message.reply_to_message,'text','                           ')
+        if (txt.find("rename")>-1) and (len(txt[txt.find("rename")+7:]) >0) and (os.path.isfile(local_file_name)):
             rename_text=txt[txt.find("rename")+7:]
             print("BAJ LocFileName Before : "+local_file_name)
             absName=os.path.join(os.path.dirname(local_file_name),rename_text+Path(local_file_name).suffix)
